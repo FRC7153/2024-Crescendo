584883e034fb3387bf98759fd9ddcfc34133d5e0
braykoff <99614905+Braykoff@users.noreply.github.com>
2024-01-27 15:59:43 -0500

    Fixed Intake logging issue

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -45,13 +45,13 @@ public class Intake implements Subsystem {
     /** Runs the intake forward */
     public void enableIntake() {
         intakeController.setReference(IntakeConstants.kINTAKE_SETPOINT / IntakeConstants.kINTAKE_RATIO, ControlType.kVelocity, 0);
-        intakeSetpointLog.append(IntakeConstants.kINTAKE_SETPOINT / IntakeConstants.kINTAKE_RATIO);
+        intakeSetpointLog.append(IntakeConstants.kINTAKE_SETPOINT);
     }
 
     /** Runs the intake in reverse */
     public void reverseIntake() {
         intakeController.setReference(IntakeConstants.kINTAKE_SETPOINT / IntakeConstants.kINTAKE_RATIO * -1.0, ControlType.kVelocity, 0);
-        intakeSetpointLog.append(IntakeConstants.kINTAKE_SETPOINT / IntakeConstants.kINTAKE_RATIO * -1.0);
+        intakeSetpointLog.append(IntakeConstants.kINTAKE_SETPOINT * -1.0);
     }
 
     /** Ends the intake function */
