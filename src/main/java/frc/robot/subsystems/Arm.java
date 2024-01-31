25218c7b8b7620f3972134fc45d236f74de4c5ff
mualexx <alexmusiitwa@gmail.com>
2024-01-30 20:25:19 -0500

    Added arm and climber file. Made changes to constants, arm, and climber file.

---
+++ src/main/java/frc/robot/subsystems/Arm.java
@@ -0,0 +1,18 @@
+package frc.robot.subsystems;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+
+import edu.wpi.first.util.datalog.DoubleLogEntry;
+import edu.wpi.first.wpilibj.DataLogManager;
+import edu.wpi.first.wpilibj2.command.Subsystem;
+import frc.robot.Constants.HardwareConstants;
+
+
+
+public class Arm {
+    public CANSparkMax lowerPivot = new CANSparkMax(HardwareConstants.kLOWER_PIVOT_CAN, MotorType.kBrushless);
+    public CANSparkMax upperPivot = new CANSparkMax(HardwareConstants.kUPPER_PIVOT_CAN, MotorType.kBrushless);
+
+    
+}
